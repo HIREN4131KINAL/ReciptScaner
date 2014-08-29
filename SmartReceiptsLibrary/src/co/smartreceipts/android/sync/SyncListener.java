e01f866ea487daed51153b4291e855506b830ead
Will Baumann <wbaumann@github.com>
2014-08-29 09:55:50 -0400

    Began creating a sync interface framework

--- SmartReceiptsLibrary/src/co/smartreceipts/android/sync/SyncListener.java
+++ SmartReceiptsLibrary/src/co/smartreceipts/android/sync/SyncListener.java
@@ -1,5 +1,30 @@
 package co.smartreceipts.android.sync;
 
-public interface SyncListener {
+/**
+ * This interface defines a series of callback mechanisms that can be used to track when our data has been successfully
+ * synchronized or not
+ * 
+ * @author Will Baumann
+ */
+public interface SyncListener<T extends SyncKey> {
 
+	/**
+	 * This method will be called whenever our data has successfully synchronized with the back-end
+	 * 
+	 * @param type
+	 *            - the {@link SyncRequestType} of the request
+	 * @param result
+	 *            - the {@link SyncResult} that was received
+	 */
+	public void onSyncSuccess(SyncRequestType type, SyncResult<T> result);
+
+	/**
+	 * This method will be called whenever our data has failed to synchronize with the back-end
+	 * 
+	 * @param type
+	 *            - the {@link SyncRequestType} of the request
+	 * @param exception
+	 *            - the {@link SyncException}, which details why the sync failed
+	 */
+	public void onSyncError(SyncRequestType type, SyncException exception);
 }
--- SmartReceiptsLibrary/src/co/smartreceipts/android/sync/SyncProvider.java
+++ SmartReceiptsLibrary/src/co/smartreceipts/android/sync/SyncProvider.java
@@ -1,85 +1,14 @@
 package co.smartreceipts.android.sync;
 
-import java.sql.Date;
-
-import co.smartreceipts.android.model.ReceiptRow;
-import co.smartreceipts.android.model.TripRow;
-
-public interface SyncProvider {
+public interface SyncProvider<T extends SyncKey> extends NetworkStateChangeListener {
 
 	/**
-	 * Registers a {@link SyncListener} instance with this {@link SyncProvider} implementation
+	 * Determines if we can use this provider to support this particular category of synchronization operations
 	 * 
-	 * @param listener
-	 *            - the {@link SyncListener} to alert when certain events complete
+	 * @param request
+	 *            - the request to check
+	 * @return {@code true} if this is supported. {@code false} otherwise.
 	 */
-	public void registerListener(SyncListener listener);
-
-	/**
-	 * Queries all trips that are currently available for the current {@link SyncUser}
-	 */
-	public void queryTrips();
-
-	/**
-	 * Queries all trips that occur after a given date for the current {@link SyncUser}
-	 * 
-	 * @param - The {@link Date} to search after
-	 */
-	public void queryTripsAfter(Date date);
-
-	/**
-	 * Uploads a trip to the sync provider for the current {@link SyncUser}
-	 * 
-	 * @param trip
-	 *            - the {@link TripRow} to insert
-	 */
-	public void insertTrip(TripRow trip);
-
-	/**
-	 * Updates a trip that current exists in the current {@link SyncProvider} implementation or uploads if it does not
-	 * already exist.
-	 * 
-	 * @param trip
-	 *            - the {@link TripRow} to update
-	 */
-	public void updateTrip(TripRow trip);
-
-	/**
-	 * Removes a trip from trip the current {@link SyncProvider} implementation
-	 * 
-	 * @param trip
-	 *            - the {@link TripRow} to delete
-	 */
-	public void deleteTrip(TripRow trip);
-
-	/**
-	 * Queries all receipts that are currently available for the current {@link SyncUser}
-	 */
-	public void queryReceipts();
-
-	/**
-	 * Uploads a receipt to the sync provider for the current {@link SyncUser}
-	 * 
-	 * @param receipt
-	 *            - the {@link ReceiptRow} to insert
-	 */
-	public void insertReceipt(ReceiptRow receipt);
-
-	/**
-	 * Updates a receipt that current exists in the current {@link SyncProvider} implementation or uploads if it does
-	 * not already exist.
-	 * 
-	 * @param receipt
-	 *            - the {@link ReceiptRow} to update
-	 */
-	public void updateReceipt(ReceiptRow receipt);
-
-	/**
-	 * Removes a receipt from receipt the current {@link SyncProvider} implementation
-	 * 
-	 * @param receipt
-	 *            - the {@link ReceiptRow} to delete
-	 */
-	public void deleteReceipt(ReceiptRow receipt);
+	public boolean supportsSynchronization(SyncRequest<T> request);
 
 }

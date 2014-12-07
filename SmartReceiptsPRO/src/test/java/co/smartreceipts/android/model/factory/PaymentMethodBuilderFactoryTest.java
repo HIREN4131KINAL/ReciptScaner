8e83dbe197a0b0c03e2c740e740d6dbabd8f4fe8
Will Baumann <wbaumann@github.com>
2014-12-07 15:19:41 -0500

    Updated PaymentMethod model

---
+++ SmartReceiptsPRO/src/test/java/co/smartreceipts/android/model/factory/PaymentMethodBuilderFactoryTest.java
@@ -0,0 +1,43 @@
+package co.smartreceipts.android.model.factory;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import co.smartreceipts.android.utils.PaymentMethodUtils;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+@Config(emulateSdk = 18)
+@RunWith(RobolectricTestRunner.class)
+public class PaymentMethodBuilderFactoryTest {
+
+    private PaymentMethodBuilderFactory builderFactory;
+
+    @Before
+    public void setUp() throws Exception {
+        builderFactory = new PaymentMethodBuilderFactory();
+    }
+
+    @Test
+    public void testId() {
+        builderFactory.setId(PaymentMethodUtils.Constants.ID);
+        assertEquals(PaymentMethodUtils.Constants.ID, builderFactory.build().getId());
+    }
+
+    @Test
+    public void testMethod() {
+        builderFactory.setMethod(PaymentMethodUtils.Constants.METHOD);
+        assertEquals(PaymentMethodUtils.Constants.METHOD, builderFactory.build().getMethod());
+    }
+
+    @Test
+    public void testEmptyBuilder() {
+        assertTrue(builderFactory.build().getId() < 0);
+        assertNotNull(builderFactory.build().getMethod());
+    }
+}
---
+++ SmartReceiptsPRO/src/test/java/co/smartreceipts/android/utils/PaymentMethodUtils.java
@@ -0,0 +1,12 @@
+package co.smartreceipts.android.utils;
+
+/**
+ * Created by williambaumann on 12/5/14.
+ */
+public class PaymentMethodUtils {
+
+    public static final class Constants {
+        public static final int ID = 101;
+        public static final String METHOD = "method";
+    }
+}

76fe0897ce005ab9a82d0297a623d28b19b8a77c
Will Baumann <wbaumann@github.com>
2015-05-13 21:33:30 -0400

    Started creating an edit/new receipt layout

---
+++ SmartReceiptsLibrary/src/main/java/co/smartreceipts/android/fragments/UpdateReceiptFragment.java
@@ -0,0 +1,8 @@
+package co.smartreceipts.android.fragments;
+
+public class UpdateReceiptFragment extends WBFragment {
+
+    public static final String TAG = UpdateReceiptFragment.class.getName();
+
+
+}

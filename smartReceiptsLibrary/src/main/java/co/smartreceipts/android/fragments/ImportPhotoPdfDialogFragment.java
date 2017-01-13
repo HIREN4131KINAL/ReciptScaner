856e40d194698e2d47c6ab6001022c9767ebee3a
Will Baumann <wbaumann@github.com>
2017-01-13 00:01:39 -0500

    Addressed a crash when the user doesn't have a valid 'import' app

--- smartReceiptsLibrary/src/main/java/co/smartreceipts/android/fragments/ImportPhotoPdfDialogFragment.java
+++ smartReceiptsLibrary/src/main/java/co/smartreceipts/android/fragments/ImportPhotoPdfDialogFragment.java
@@ -2,12 +2,14 @@ package co.smartreceipts.android.fragments;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.widget.Toast;
 
 import co.smartreceipts.android.R;
 import co.smartreceipts.android.imports.RequestCodes;
@@ -35,14 +37,18 @@ public class ImportPhotoPdfDialogFragment extends DialogFragment implements Dial
     public void onClick(DialogInterface dialogInterface, int which) {
         // @see https://developer.android.com/guide/topics/providers/document-provider.html#client
         // Use ACTION_GET_CONTENT instead of ACTION_OPEN_DOCUMENT as this is simply a read/import
-        final Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.addCategory(Intent.CATEGORY_OPENABLE);
-        if (which == WHICH_IMAGE) {
-            intent.setType("image/*");
-            getParentFragment().startActivityForResult(intent, RequestCodes.IMPORT_GALLERY_IMAGE);
-        } else if (which == WHICH_PDF) {
-            intent.setType("application/pdf");
-            getParentFragment().startActivityForResult(intent, RequestCodes.IMPORT_GALLERY_PDF);
+        try {
+            final Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            if (which == WHICH_IMAGE) {
+                intent.setType("image/*");
+                getParentFragment().startActivityForResult(intent, RequestCodes.IMPORT_GALLERY_IMAGE);
+            } else if (which == WHICH_PDF) {
+                intent.setType("application/pdf");
+                getParentFragment().startActivityForResult(intent, RequestCodes.IMPORT_GALLERY_PDF);
+            }
+        } catch (ActivityNotFoundException ex) {
+            Toast.makeText(getContext(), getString(R.string.error_no_file_intent_dialog_title), Toast.LENGTH_SHORT).show();
         }
         dismiss();
     }

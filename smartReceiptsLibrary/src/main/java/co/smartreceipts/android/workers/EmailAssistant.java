e700354f32efb29a1043c7352e7c47f825dd84b2
apapad <aristides.papadopoulos@gmail.com>
2017-02-09 16:38:59 +0100

    Removed itext jar and unsused references to it.

--- smartReceiptsLibrary/src/main/java/co/smartreceipts/android/workers/EmailAssistant.java
+++ smartReceiptsLibrary/src/main/java/co/smartreceipts/android/workers/EmailAssistant.java
@@ -1,6 +1,5 @@
 package co.smartreceipts.android.workers;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.ActivityNotFoundException;
@@ -17,31 +16,9 @@ import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
-import android.text.TextUtils;
 import android.widget.Toast;
 
-import com.itextpdf.text.BadElementException;
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Element;
-import com.itextpdf.text.Font;
-import com.itextpdf.text.FontFactory;
-import com.itextpdf.text.Image;
-import com.itextpdf.text.PageSize;
-import com.itextpdf.text.Phrase;
-import com.itextpdf.text.Rectangle;
-import com.itextpdf.text.pdf.BadPdfFormatException;
-import com.itextpdf.text.pdf.ColumnText;
-import com.itextpdf.text.pdf.PdfPCell;
-import com.itextpdf.text.pdf.PdfPTable;
-import com.itextpdf.text.pdf.PdfPageEventHelper;
-import com.itextpdf.text.pdf.PdfReader;
-import com.itextpdf.text.pdf.PdfWriter;
-
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -92,8 +69,6 @@ public class EmailAssistant {
             return this.index;
         }
     }
-
-    private static final Rectangle DEFAULT_PAGE_SIZE = PageSize.A4;
 
     private final Context mContext;
     private final Flex mFlex;
@@ -499,226 +474,6 @@ public class EmailAssistant {
             return brush.getFontSpacing();
         }
 
-        private static final float BIG_COLUMN_DIVIDER = 2.1f;
-
-        private Document addImageRows(Document document, List<Receipt> receipts, PdfWriter writer) {
-            // Set up
-            PdfPTable table = getPanedPdfPTable();
-            final int size = receipts.size();
-            Receipt receipt;
-            Image img1 = null, img2 = null;
-            Receipt receipt1 = null; // Tracks the receipt in the left column (if any)
-            int flag = 0;
-            boolean hitFirstNonFullPage = false;
-            final ArrayList<Receipt> fullpageReceipts = new ArrayList<Receipt>(); //Includes Full Page Images && PDFs
-
-            for (int i = 0; i < size; i++) {
-                receipt = receipts.get(i);
-                if (filterOutReceipt(mPreferences, receipt)) { // Don't include receipts that have been explicitly filtered out
-                    continue;
-                }
-                if (receipt.isFullPage() || receipt.hasPDF()) { // Don't include full page or PDFs yet (add at the end)
-                    if (!hitFirstNonFullPage) {
-                        addFullPageImage(document, receipt, writer); // If it's fullpage first, add immediately
-                    } else {
-                        fullpageReceipts.add(receipt); // Else, add to the end
-                    }
-                    continue;
-                }
-                if (!receipt.hasImage()) { // Don't include receipts without images (called after the PDF line, b/c PDFs shouldn't be removed)
-                    continue;
-                }
-                hitFirstNonFullPage = true;
-                if (receipt.hasImage() && img1 == null) {
-                    try {
-                        img1 = Image.getInstance(receipt.getFilePath());
-                        receipt1 = receipt;
-                    } catch (Exception e) {
-                        Logger.error(this, e);
-                        continue;
-                    }
-                } else if (receipt.hasImage() && img2 == null) {
-                    try {
-                        img2 = Image.getInstance(receipt.getFilePath());
-                    } catch (Exception e) {
-                        Logger.error(this, e);
-                        continue;
-                    }
-                    addHeaderCell(table, receipt1);
-                    table.addCell("");
-                    addHeaderCell(table, receipt);
-                    table.addCell(getCell(img1));
-                    table.addCell("");
-                    table.addCell(getCell(img2));
-                    table.setSpacingAfter(40);
-                    img1 = null;
-                    img2 = null;
-                    if (++flag == 2) {//ugly hack to fix how page breaks are separated
-                        table.completeRow();
-                        try {
-                            document.add(table);
-                        } catch (DocumentException e) {
-                            Logger.error(this, e);
-                        }
-                        table = getPanedPdfPTable();
-                        flag = 0;
-                    }
-                }
-            }
-            if (img1 != null) {
-                addHeaderCell(table, receipt1);
-                table.addCell(" ");
-                table.addCell(" ");
-                table.addCell(getCell(img1));
-            }
-            table.completeRow();
-            try {
-                document.add(table);
-            } catch (DocumentException e) {
-                Logger.error(this, e);
-            }
-            document.newPage(); //TODO: See if this code can be made more linear (i.e. add columns dyanmically instead of doing it all at once)
-
-            //Full Page Stuff Below
-            for (int i = 0; i < fullpageReceipts.size(); i++) {
-                receipt = fullpageReceipts.get(i);
-                addFullPageImage(document, receipt, writer);
-            }
-            return document;
-        }
-
-        private void addHeaderCell(PdfPTable table, Receipt receipt) {
-            final int num = (mPreferences.includeReceiptIdInsteadOfIndexByPhoto()) ? receipt.getId() : receipt.getIndex();
-            final String extra = (mPreferences.getIncludeCommentByReceiptPhoto() && !TextUtils.isEmpty(receipt.getComment())) ? "  \u2022  " + receipt.getComment() : "";
-            table.addCell(num + "  \u2022  " + receipt.getName() + "  \u2022  " + receipt.getFormattedDate(mContext, mPreferences.getDateSeparator()) + extra);
-        }
-
-        private void addFullPageImage(Document document, Receipt receipt, PdfWriter writer) {
-            if (mPreferences.onlyIncludeReimbursableReceiptsInReports() && !receipt.isReimbursable()) {
-                return;
-            }
-            if (receipt.getPrice().getPriceAsFloat() < mPreferences.getMinimumReceiptPriceToIncludeInReports()) {
-                return;
-            }
-            PdfPTable table;
-            try {
-                if (receipt.hasPDF()) {
-                    PdfReader reader = new PdfReader(new FileInputStream(receipt.getFile()));
-                    PdfReader.unethicalreading = true; // Reduce errors due to "owner password not found"
-                    int numPages = reader.getNumberOfPages();
-                    for (int page = 0; page < numPages; ) {
-                        table = getSingleElementTable();
-                        addHeaderCell(table, receipt);
-                        try {
-                            table.addCell(Image.getInstance(writer.getImportedPage(reader, ++page)));
-                        } catch (RuntimeException e) {
-                            if (mContext != null) {
-                                // TODO: Move this check up a level (i.e. to our model/attachment), so we can alert the user in advance
-                                if (mContext instanceof Activity) {
-                                    // TODO: Figure out a real reporting mechanism, so we're not just randomly throwing out toast messages
-                                    Activity activity = (Activity) mContext;
-                                    activity.runOnUiThread(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            Toast.makeText(mContext, mContext.getString(R.string.toast_error_password_protected_pdf), Toast.LENGTH_SHORT).show();
-                                        }
-                                    });
-                                }
-                            }
-                            break;
-                        }
-                        table.completeRow();
-                        document.add(table);
-                        document.newPage();
-                    }
-                    writer.freeReader(reader);
-                    reader = null; //
-                } else if (receipt.isFullPage() && receipt.hasFile()) {
-                    table = getSingleElementTable();
-                    addHeaderCell(table, receipt);
-                    table.addCell(getFullCell(Image.getInstance(receipt.getFilePath())));
-                    table.completeRow();
-                    document.add(table);
-                    document.newPage();
-                }
-            } catch (FileNotFoundException e) {
-                Logger.error(this, e);
-                return;
-            } catch (BadPdfFormatException e) {
-                Logger.error(this, e);
-                return;
-            } catch (IOException e) {
-                Logger.error(this, e);
-                return;
-            } catch (BadElementException e) {
-                Logger.error(this, e);
-                return;
-            } catch (DocumentException e) {
-                Logger.error(this, e);
-                return;
-            }
-        }
-
-        private PdfPTable getSingleElementTable() {
-            PdfPTable table = new PdfPTable(1);
-            table.getDefaultCell().disableBorderSide(PdfPCell.LEFT);
-            table.getDefaultCell().disableBorderSide(PdfPCell.RIGHT);
-            table.getDefaultCell().disableBorderSide(PdfPCell.TOP);
-            table.getDefaultCell().disableBorderSide(PdfPCell.BOTTOM);
-            table.setWidthPercentage(100);
-            table.getDefaultCell().setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.getDefaultCell().setVerticalAlignment(Element.ALIGN_CENTER);
-            table.setSplitLate(false);
-            return table;
-        }
-
-        private PdfPTable getPanedPdfPTable() {
-            // Here we create a table with 3 columns - col 1 holds img 1, col 2 is a divider, col 3 is img 2
-            PdfPTable table = new PdfPTable(3);
-            table.getDefaultCell().disableBorderSide(PdfPCell.LEFT);
-            table.getDefaultCell().disableBorderSide(PdfPCell.RIGHT);
-            table.getDefaultCell().disableBorderSide(PdfPCell.TOP);
-            table.getDefaultCell().disableBorderSide(PdfPCell.BOTTOM);
-            table.getDefaultCell().setBorder(Rectangle.NO_BORDER);
-            table.setWidthPercentage(100);
-            float big = DEFAULT_PAGE_SIZE.getWidth() / BIG_COLUMN_DIVIDER;
-            float small = DEFAULT_PAGE_SIZE.getWidth() - 2 * big;
-            try {
-                table.setWidths(new float[]{big, small, big});
-            } catch (DocumentException e1) {
-                Logger.error(this, e1);
-            }
-            table.getDefaultCell().setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.getDefaultCell().setVerticalAlignment(Element.ALIGN_CENTER);
-            table.setSplitLate(false);
-            return table;
-        }
-
-        private final PdfPCell getCell(Image img) {
-            PdfPCell cell = new PdfPCell(img, true);
-            cell.disableBorderSide(PdfPCell.LEFT);
-            cell.disableBorderSide(PdfPCell.RIGHT);
-            cell.disableBorderSide(PdfPCell.TOP);
-            cell.disableBorderSide(PdfPCell.BOTTOM);
-            cell.setBorder(Rectangle.NO_BORDER);
-            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            cell.setVerticalAlignment(Element.ALIGN_CENTER);
-            cell.setFixedHeight(DEFAULT_PAGE_SIZE.getHeight() / 2.4f);
-            return cell;
-        }
-
-        private final PdfPCell getFullCell(Image img) {
-            PdfPCell cell = new PdfPCell(img, true);
-            cell.disableBorderSide(PdfPCell.LEFT);
-            cell.disableBorderSide(PdfPCell.RIGHT);
-            cell.disableBorderSide(PdfPCell.TOP);
-            cell.disableBorderSide(PdfPCell.BOTTOM);
-            cell.setBorder(Rectangle.NO_BORDER);
-            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            cell.setVerticalAlignment(Element.ALIGN_CENTER);
-            cell.setFixedHeight(DEFAULT_PAGE_SIZE.getHeight() / 1.15f);
-            return cell;
-        }
 
         @Override
         protected void onPostExecute(WriterResults result) {
@@ -736,15 +491,6 @@ public class EmailAssistant {
             if (memoryErrorOccured) {
                 memoryErrorOccured = false;
                 Toast.makeText(mContext, "Error: Not enough memory to stamp the images. Try stopping some other apps and try again.", Toast.LENGTH_LONG).show();
-            }
-        }
-
-        private class Footer extends PdfPageEventHelper {
-            @Override
-            public void onEndPage(PdfWriter writer, Document document) {
-                Rectangle rect = writer.getPageSize();
-                ColumnText.showTextAligned(writer.getDirectContent(), Element.ALIGN_LEFT,
-                        new Phrase(mPreferences.getPdfFooterText(), FontFactory.getFont("Times-Roman", 9, Font.ITALIC)), rect.getLeft() + 36, rect.getBottom() + 36, 0);
             }
         }
 

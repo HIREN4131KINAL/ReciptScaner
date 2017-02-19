0d571c0aa058d36c0e27b24a999d625b322742ab
Will Baumann <wbaumann@github.com>
2017-02-19 17:05:54 -0500

    Revert "Improved support for backup sync edge conditions for our receipts, since these don't trigger onInsert/update flows"
    Doing this to avoid some timing issues that seem to be arising. I'll handle this more gracefully as part of a future refactor
    
    This reverts commit 566ebc450847f83efebd3b607b066f8bee8ea384.

--- smartReceiptsLibrary/src/main/java/co/smartreceipts/android/sync/drive/listeners/ReceiptBackupListener.java
+++ smartReceiptsLibrary/src/main/java/co/smartreceipts/android/sync/drive/listeners/ReceiptBackupListener.java
@@ -5,90 +5,52 @@ import android.support.annotation.Nullable;
 
 import com.google.common.base.Preconditions;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import co.smartreceipts.android.model.Receipt;
-import co.smartreceipts.android.model.Trip;
-import co.smartreceipts.android.persistence.database.controllers.ReceiptTableEventsListener;
+import co.smartreceipts.android.model.factory.ReceiptBuilderFactory;
+import co.smartreceipts.android.persistence.database.controllers.impl.ReceiptTableController;
 import co.smartreceipts.android.persistence.database.controllers.impl.StubTableEventsListener;
 import co.smartreceipts.android.persistence.database.operations.DatabaseOperationMetadata;
 import co.smartreceipts.android.persistence.database.operations.OperationFamilyType;
 import co.smartreceipts.android.sync.drive.managers.DriveDatabaseManager;
 import co.smartreceipts.android.sync.drive.managers.DriveReceiptsManager;
+import co.smartreceipts.android.sync.drive.rx.DriveStreamsManager;
+import co.smartreceipts.android.sync.model.SyncState;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
-public class ReceiptBackupListener extends StubTableEventsListener<Receipt> implements ReceiptTableEventsListener {
+public class ReceiptBackupListener extends DatabaseBackupListener<Receipt> {
 
-    private final DriveDatabaseManager driveDatabaseManager;
-    private final DriveReceiptsManager driveReceiptsManager;
+    private final DriveReceiptsManager mDriveReceiptsManager;
 
     public ReceiptBackupListener(@NonNull DriveDatabaseManager driveDatabaseManager, @NonNull DriveReceiptsManager driveReceiptsManager) {
-        this.driveDatabaseManager = Preconditions.checkNotNull(driveDatabaseManager);
-        this.driveReceiptsManager = Preconditions.checkNotNull(driveReceiptsManager);
+        super(driveDatabaseManager);
+        mDriveReceiptsManager = Preconditions.checkNotNull(driveReceiptsManager);
     }
 
     @Override
     public void onInsertSuccess(@NonNull Receipt receipt, @NonNull DatabaseOperationMetadata databaseOperationMetadata) {
+        super.onInsertSuccess(receipt, databaseOperationMetadata);
         if (databaseOperationMetadata.getOperationFamilyType() != OperationFamilyType.Sync) {
-            driveDatabaseManager.syncDatabase();
-            driveReceiptsManager.handleInsertOrUpdate(receipt);
+            mDriveReceiptsManager.handleInsertOrUpdate(receipt);
         }
     }
 
     @Override
     public void onUpdateSuccess(@NonNull Receipt oldReceipt, @NonNull Receipt newReceipt, @NonNull DatabaseOperationMetadata databaseOperationMetadata) {
+        super.onUpdateSuccess(oldReceipt, newReceipt, databaseOperationMetadata);
         if (databaseOperationMetadata.getOperationFamilyType() != OperationFamilyType.Sync) {
-            driveDatabaseManager.syncDatabase();
-            driveReceiptsManager.handleInsertOrUpdate(newReceipt);
+            mDriveReceiptsManager.handleInsertOrUpdate(newReceipt);
         }
     }
 
     @Override
     public void onDeleteSuccess(@NonNull Receipt receipt, @NonNull DatabaseOperationMetadata databaseOperationMetadata) {
+        super.onDeleteSuccess(receipt, databaseOperationMetadata);
         if (databaseOperationMetadata.getOperationFamilyType() != OperationFamilyType.Sync) {
-            driveDatabaseManager.syncDatabase();
-            driveReceiptsManager.handleDelete(receipt);
+            mDriveReceiptsManager.handleDelete(receipt);
         }
-    }
-
-    @Override
-    public void onMoveSuccess(@NonNull Receipt oldReceipt, @NonNull Receipt newReceipt) {
-        driveDatabaseManager.syncDatabase();
-        driveReceiptsManager.handleInsertOrUpdate(newReceipt);
-    }
-
-    @Override
-    public void onMoveFailure(@NonNull Receipt oldReceipt, @Nullable Throwable e) {
-
-    }
-
-    @Override
-    public void onCopySuccess(@NonNull Receipt oldReceipt, @NonNull Receipt newReceipt) {
-        driveDatabaseManager.syncDatabase();
-        driveReceiptsManager.handleInsertOrUpdate(newReceipt);
-    }
-
-    @Override
-    public void onCopyFailure(@NonNull Receipt oldReceipt, @Nullable Throwable e) {
-
-    }
-
-    @Override
-    public void onSwapSuccess() {
-        driveReceiptsManager.initialize();
-    }
-
-    @Override
-    public void onSwapFailure(@Nullable Throwable e) {
-
-    }
-
-    @Override
-    public void onGetSuccess(@NonNull List<Receipt> list, @NonNull Trip trip) {
-
-    }
-
-    @Override
-    public void onGetFailure(@Nullable Throwable e, @NonNull Trip trip) {
-
     }
 }

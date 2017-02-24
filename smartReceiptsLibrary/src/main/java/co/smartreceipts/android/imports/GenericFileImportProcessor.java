748030149e5510b1d6ea2ee3cc935e32e8f2ce77
Will Baumann <wbaumann@github.com>
2017-02-24 16:39:57 -0500

    Fixed a bug in which we could accidentally delete the files we were importing if they weren't context-schemed. Also added UTs to the import flows

--- smartReceiptsLibrary/src/main/java/co/smartreceipts/android/imports/GenericFileImportProcessor.java
+++ smartReceiptsLibrary/src/main/java/co/smartreceipts/android/imports/GenericFileImportProcessor.java
@@ -4,7 +4,6 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.net.Uri;
 import android.support.annotation.NonNull;
-import android.webkit.MimeTypeMap;
 
 import com.google.common.base.Preconditions;
 
@@ -22,18 +21,18 @@ import wb.android.storage.StorageManager;
 
 public class GenericFileImportProcessor implements FileImportProcessor {
 
-    private final Trip mTrip;
-    private final StorageManager mStorageManner;
-    private final ContentResolver mContentResolver;
+    private final Trip trip;
+    private final StorageManager storageManner;
+    private final ContentResolver contentResolver;
 
     public GenericFileImportProcessor(@NonNull Trip trip, @NonNull StorageManager storageManager, @NonNull Context context) {
         this(trip, storageManager, context.getContentResolver());
     }
 
     public GenericFileImportProcessor(@NonNull Trip trip, @NonNull StorageManager storageManager, @NonNull ContentResolver contentResolver) {
-        mTrip = Preconditions.checkNotNull(trip);
-        mStorageManner = Preconditions.checkNotNull(storageManager);
-        mContentResolver = Preconditions.checkNotNull(contentResolver);
+        this.trip = Preconditions.checkNotNull(trip);
+        this.storageManner = Preconditions.checkNotNull(storageManager);
+        this.contentResolver = Preconditions.checkNotNull(contentResolver);
     }
 
     @NonNull
@@ -45,9 +44,9 @@ public class GenericFileImportProcessor implements FileImportProcessor {
             public void call(Subscriber<? super File> subscriber) {
                 InputStream inputStream = null;
                 try {
-                    inputStream = mContentResolver.openInputStream(uri);
-                    final File destination = mStorageManner.getFile(mTrip.getDirectory(), System.currentTimeMillis() + "." + UriUtils.getExtension(uri, mContentResolver));
-                    if (mStorageManner.copy(inputStream, destination, true)) {
+                    inputStream = contentResolver.openInputStream(uri);
+                    final File destination = storageManner.getFile(trip.getDirectory(), System.currentTimeMillis() + "." + UriUtils.getExtension(uri, contentResolver));
+                    if (storageManner.copy(inputStream, destination, true)) {
                         subscriber.onNext(destination);
                         subscriber.onCompleted();
                         Logger.info(GenericFileImportProcessor.this, "Successfully copied Uri to the Smart Receipts directory");
